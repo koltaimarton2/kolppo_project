0d615f80296972839c2a3cfde1701b255b1d2547
lipotjoci <unconfigured@null.spigotmc.org>
2023-10-12 23:25:25 +0200

    Hátizsák GUI hozzáadva.

--- Player.py
+++ Player.py
@@ -2,17 +2,102 @@ from pygame import Vector2
 
 class Player:
     def __init__(self):
-        self.RUN = False
         self.SPEED = 150
+        self.HP = 100
+        self.BALANCE = 1000
+        self.XP = 2
+        self.LEVEL = 1
+        self.HONOR = 50
         self.RUN = False
         self.PLAYER_POS = Vector2()
-    def setSpeed(self, amount):
-        self.SPEED = amount
+        self.FROZEN = False
+        self.INVENTORY = {"Fél Személyi" : 1, "Csoki" : 2}
+        self.INVENTORYKeys = []
+        self.INVENTORYValues = []
+        self.HandleInventoryUpdate()
+
+    # ------------------------------------------ Get Stuff
     def getSpeed(self):
         return self.SPEED
-    def setRunning(self, amount):
-        self.RUN = amount
+    def getInventory(self):
+        return self.INVENTORY
+    def getInventoryItems(self) -> list:
+        return self.INVENTORYKeys
+    def getInventoryAmounts(self, key:int) -> list:
+        return self.INVENTORYValues[key]
+    def getHP(self) -> int:
+        return self.HP
+    def getXP(self) -> int:
+        return self.XP
+    def getLevel(self) -> int:
+        return self.LEVEL
+    def getHonor(self) -> int:
+        return self.HONOR
+    def getPlayerPos(self) -> int:
+        return self.PLAYER_POS
+
+    # ------------------------------------------ Set Stuff
+
+    def setSpeed(self, amount) -> None:
+        self.SPEED = amount
+    
+    def setXP(self, amount) -> None:
+        self.XP = amount
+
+    def damage(self, amount) -> bool:
+        self.HP -= amount
+        if(self.HP <= 0):
+            return True
+        else: return False
+
+    def setRunning(self) -> None:
+        self.RUN = not self.RUN
         self.isRun()
-    def isRun(self):
+
+    def isRun(self) -> None:
         if(self.RUN): self.SPEED = 300
         else: self.SPEED = 150
+    
+    # ------------------------------------------ Add to Stuff
+
+    def addToInventory(self, item:str) -> None:
+        self.HandleInventoryUpdate()
+        keys = self.INVENTORYKeys
+        for i in range(0, len(keys)):
+            if(item == keys[i]):
+                self.INVENTORY[item] += 1
+            else:
+                self.INVENTORY[item] = 1
+                i = len(keys)
+
+    def addXP(self, amount) -> None:
+        self.XP += amount
+        self.checkForLevelUp()
+
+    def Heal(self, amount):
+        if(not ((self.HP+amount) > 100)):
+            self.HP += amount
+        else: self.HP = 100
+
+    # ------------------------------------------ Handle Stuff
+
+    def useItem(self, item:str) -> None:
+        if((self.INVENTORY[item]-1) == 0):
+            self.INVENTORY.pop(item)
+        else:
+            self.INVENTORY[item] -= 1
+        self.HandleInventoryUpdate()
+
+    def checkForLevelUp(self) -> None:
+        if(self.XP > self.nextLevel):
+            self.LEVEL += 1
+            self.setXP(self.XP - self.nextLevel)
+
+    def HandleInventoryUpdate(self) -> None:
+        self.INVENTORYKeys = list(self.INVENTORY.keys())
+        self.INVENTORYValues = list(self.INVENTORY.values())
+
+if __name__ == "__main__":
+    frigyes = Player()
+    inventory = frigyes.getInventory()
+    print(inventory[0, 0])
--- mainGame.py
+++ mainGame.py
@@ -5,43 +5,109 @@ class kollpoCity:
         pygame.init()
         self.SCREENSIZE = (800, 800)
         self.SCREEN = pygame.display.set_mode(self.SCREENSIZE)
+        self.selectedItem = 0
+        pygame.display.set_caption('Kolppo')
         self.CLOCK = pygame.time.Clock()
+        self.RUNINSTANCE = True
+        self.SCREENTYPE = 0
+        self.FONT = pygame.font.Font('fonts/pixel.ttf', 32)
         self.DeltaTime = 0
         self.Frigyes = Player()
         self.Frigyes.PLAYER_POS = pygame.Vector2(self.SCREEN.get_width() / 2, self.SCREEN.get_height() / 2)
 
     def Start(self):
-        running = True
         if(not pygame.get_init()): return
-        font = pygame.font.Font()
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-            self.SCREEN.fill("white")
-
-            pygame.draw.circle(self.SCREEN, "red", self.Frigyes.PLAYER_POS, 20)
-
+        while self.RUNINSTANCE:
             keys = pygame.key.get_pressed()
-            if keys[pygame.K_w]:
-                self.Frigyes.PLAYER_POS.y -= self.Frigyes.getSpeed() * self.DeltaTime
-            if keys[pygame.K_s]:
-                self.Frigyes.PLAYER_POS.y += self.Frigyes.getSpeed() * self.DeltaTime
-            if keys[pygame.K_a]:
-                self.Frigyes.PLAYER_POS.x -= self.Frigyes.getSpeed() * self.DeltaTime
-            if keys[pygame.K_d]:
-                self.Frigyes.PLAYER_POS.x += self.Frigyes.getSpeed() * self.DeltaTime
-            if keys[pygame.K_LSHIFT]: 
-                # print(self.Frigyes.RUN)
-                self.Frigyes.setRunning(True)
-            else:
-                # print(self.Frigyes.RUN)
-                self.Frigyes.setRunning(False)
-
+            self.HandleInput(keys)
             self.DeltaTime = self.CLOCK.tick(60) / 1000
-
-            pygame.display.flip()
+            pygame.display.update()
         pygame.quit()
+
+    def HandleScreen(self):
+        match self.SCREENTYPE:
+            case 0:
+                self.Draw()
+            case 1:
+                self.escapeMenuDraw()
+            case 2:
+                self.backpackMenuDraw()
+            case 3:
+                self.mapMenuDraw()
+            case 4:
+                self.shopMenuDraw()
+            case 5:
+                self.mainMenuDraw()
+
+    def HandleInput(self, keys):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                    self.RUNINSTANCE = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE: self.isItActive(1)
+                if event.key == pygame.K_b: self.isItActive(2)
+                if event.key == pygame.K_m: self.isItActive(3)
+                if event.key == pygame.K_LSHIFT: self.Frigyes.setRunning()
+                if(self.SCREENTYPE != 0):
+                    self.selectHandle(event)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LSHIFT: self.Frigyes.setRunning()
+
+        if(self.SCREENTYPE == 0):
+            if keys[pygame.K_w]:
+                    self.Frigyes.PLAYER_POS.y -= self.Frigyes.getSpeed() * self.DeltaTime
+            if keys[pygame.K_s]:
+                    self.Frigyes.PLAYER_POS.y += self.Frigyes.getSpeed() * self.DeltaTime
+            if keys[pygame.K_a]:
+                    self.Frigyes.PLAYER_POS.x -= self.Frigyes.getSpeed() * self.DeltaTime
+            if keys[pygame.K_d]:
+                    self.Frigyes.PLAYER_POS.x += self.Frigyes.getSpeed() * self.DeltaTime
+            self.selectedItem = 0
+
+        self.HandleScreen()
+
+    def backpackMenuDraw(self):
+        self.SCREEN.fill((0, 0, 0))
+        itemText = ""
+        for idx, keys in enumerate(self.Frigyes.getInventoryItems()):
+            if (idx == self.selectedItem):
+                itemText = self.FONT.render(f'{keys}        {self.Frigyes.getInventoryAmounts(idx)}', True, (255, 255, 255))
+            else:
+                itemText = self.FONT.render(f'{keys}        {self.Frigyes.getInventoryAmounts(idx)}', True, (128, 128, 128))
+            itemRect = itemText.get_rect()
+            itemRect.center = (400, (400+(30*idx)))
+            self.SCREEN.blit(itemText, itemRect)
+        
+    def escapeMenuDraw(self):
+        self.SCREEN.fill((0, 0, 0))
+    def mapMenuDraw(self):
+        self.SCREEN.fill((0, 0, 0))
+
+    def shopMenuDraw(self):
+        self.SCREEN.fill((0, 0, 0))
+
+    def mainMenuDraw(self):
+        self.SCREEN.fill((128, 0, 0))
+    
+
+    def Draw(self):
+        self.SCREEN.fill((255, 255, 255))
+        pygame.draw.circle(self.SCREEN, "red", self.Frigyes.PLAYER_POS, 20)
+
+    def isItActive(self, Stype):
+        if(self.SCREENTYPE != Stype): self.SCREENTYPE = Stype
+        else: self.SCREENTYPE = 0
+
+    def selectHandle(self, event):
+        if event.key == pygame.K_DOWN or event.key == pygame.K_s: 
+                    if (self.selectedItem + 1) < len(self.Frigyes.getInventoryItems()): self.selectedItem += 1
+        if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    if ((self.selectedItem - 1) >= 0): self.selectedItem -= 1
+        if event.key == pygame.K_RETURN:
+            currItem = self.Frigyes.getInventoryItems()[self.selectedItem]
+            self.Frigyes.useItem(currItem)
+            self.selectedItem = 0
+            self.backpackMenuDraw()
 
 newGame = kollpoCity()
 newGame.Start()
