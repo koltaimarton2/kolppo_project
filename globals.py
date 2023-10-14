dbdc950100399990b845134219dfec09af2334e0
lipotjoci <unconfigured@null.spigotmc.org>
2023-10-14 23:56:48 +0200

    Tileset és fontsize modosítva.

--- globals.py
+++ globals.py
@@ -3,6 +3,7 @@ pygame.font.init()
 
 SCREENWIDTH, SCREENHEIGHT = 800, 800
 FPS = 60
-TILESIZE = 64
+TILESIZE = 16
 FONT = pygame.font.Font('fonts/pixel.ttf', 12)
+BIGFONT = pygame.font.Font('fonts/pixel.ttf', 24)
 deltaTime = 0.0
