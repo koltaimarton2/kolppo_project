986d03e1669bd23a0a24bff1f89eb8016c3256cd
lipotjozsef <lipot.jozsef.zoltan@students.jedlik.eu>
2023-11-08 09:51:00 +0100

    Roulette scenes hozzádva

--- Python_Game/entity.py
+++ Python_Game/entity.py
@@ -18,12 +18,27 @@ class Entity:
             self.hp = 100
 
 class Player(Entity):
-    def __init__(self, HealthPoint: int = 100):
+    def __init__(self, HealthPoint: int = 20):
         super().__init__(HealthPoint)
         self.selectedItem = 0
         self.balance = 1000
         self.hasWeapon = False
+        self.rouletteChoice = ""
+        self.rouletteAmount = 0
 
+    def getRouletteChoice(self) -> str:
+        currRouletteChoice = self.rouletteChoice
+        self.rouletteChoice = ""
+        return currRouletteChoice
+
+    def setRouletteAmount(self, amount:int) -> None:
+        self.rouletteAmount = amount
+        self.balance -= self.rouletteAmount
+
+    def getRouletteAmount(self) -> int:
+        currRouletteChoice = self.rouletteAmount
+        self.rouletteAmount = 0
+        return currRouletteChoice
 
     def getHpText(self):
         hp = "#" * int((self.hp / 5))
